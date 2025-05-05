7c401e5df4d1af26849d8be7a5db00f86fc00174
sumududias <sumududias@gmail.com>
2025-05-05 15:37:30 +0530

    Simplified admin for debugging

--- cricket_stats/admin.py
+++ cricket_stats/admin.py
@@ -7,23 +7,23 @@ class MatchPlayerInline(admin.TabularInline):
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'team', 'role', 'batting_style', 'bowling_style')
-    list_filter = ('team', 'role')
-    search_fields = ('name',)
+    list_display = ('first_name', 'last_name', 'primary_role', 'batting_style', 'bowling_style')
+    list_filter = ('primary_role', 'batting_style', 'is_active')
+    search_fields = ('first_name', 'last_name')
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('match_date', 'team1', 'team2', 'venue', 'match_type')
-    list_filter = ('match_type', 'venue')
+    list_display = ('date', 'opponent', 'venue', 'match_type', 'result')
+    list_filter = ('match_type', 'venue', 'result')
     inlines = [MatchPlayerInline]
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
+    list_display = ('name', 'season', 'coach')
+    search_fields = ('name', 'coach')
 
 @admin.register(MatchPlayer)
 class MatchPlayerAdmin(admin.ModelAdmin):
-    list_display = ('match', 'player', 'runs_scored', 'wickets_taken')
-    list_filter = ('match', 'player')
-    search_fields = ('player__name',)
+    list_display = ('match', 'player', 'runs_scored', 'wickets_taken', 'is_playing_xi')
+    list_filter = ('match', 'player', 'is_playing_xi')
+    search_fields = ('player__first_name', 'player__last_name')
