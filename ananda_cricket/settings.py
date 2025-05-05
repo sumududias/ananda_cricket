d982d2959913045d2b4d915c02cb23cbfc0d1a35
sumududias <sumududias@gmail.com>
2025-05-05 12:22:39 +0530

    Update database settings with port and charset

--- ananda_cricket/settings.py
+++ ananda_cricket/settings.py
@@ -82,8 +82,10 @@ DATABASES = {
         'USER': 'anandacricket',
         'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
         'HOST': 'anandacricket.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
         },
     }
 }
